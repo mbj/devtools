bbdcd3b58023f52300f89a10da9c6775f6b50c73
Dan Kubb <dan.kubb@gmail.com>
2013-12-27 22:11:36 -0800

    Refactor shared spec to be simpler

--- shared/spec/shared/hash_method_behavior.rb
+++ shared/spec/shared/hash_method_behavior.rb
@@ -3,11 +3,7 @@
 shared_examples_for 'a hash method' do
   it_should_behave_like 'an idempotent method'
 
-  specification = proc do
+  it 'is a Fixnum' do
     should be_instance_of(Fixnum)
-  end
-
-  it 'is a fixnum' do
-    instance_eval(&specification)
   end
 end
