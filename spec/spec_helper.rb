4da6aab76da3236bfeccd0c4b884688e32966500
Dan Kubb <dan.kubb@gmail.com>
2013-12-28 00:05:14 -0800

    Add ruby 2.1.0 to travis config
    * Fix spec configuration to work with a bundler cache

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -12,19 +12,17 @@ if ENV['COVERAGE'] == 'true'
   ]
 
   SimpleCov.start do
-    command_name     'spec:unit'
-    add_filter       'config'
-    add_filter       'spec'
+    command_name 'spec:unit'
+
+    add_filter 'config'
+    add_filter 'spec'
+    add_filter 'vendor'
+
     minimum_coverage 100
   end
 end
 
 require 'devtools'
-
-# Require spec support files and shared behavior
-Dir[File.expand_path('../{support,shared}/**/*.rb', __FILE__)].each do |file|
-  require file.chomp('.rb')
-end
 
 RSpec.configure do |config|
   config.expect_with :rspec do |expect_with|
