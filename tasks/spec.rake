mbj/devtools

commit d45bfc4594f91de8d8628a3fba42501747ab4251

Simplify spec glob expressions
* This removes nasty double slashes in globed paths.

tasks/spec.rake
@@ -11,18 +11,18 @@ begin
 
   desc 'Run all specs'
   RSpec::Core::RakeTask.new(:spec) do |task|
-    task.pattern = 'spec/{unit,integration}/{,/*/**}/*_spec.rb'
+    task.pattern = 'spec/{unit,integration}/**/*_spec.rb'
   end
 
   namespace :spec do
     desc 'Run unit specs'
     RSpec::Core::RakeTask.new(:unit) do |task|
-      task.pattern = 'spec/unit/{,/*/**}/*_spec.rb'
+      task.pattern = 'spec/unit/*/**/*_spec.rb'
     end
 
     desc 'Run integration specs'
     RSpec::Core::RakeTask.new(:integration) do |task|
-      task.pattern = 'spec/integration/{,/*/**}/*_spec.rb'
+      task.pattern = 'spec/integration/**/*_spec.rb'
     end
   end
 rescue LoadError

commit dbdb6a70d3f7dfac0784c08e25266860ecb17d76

Fixed broken globbing introduced in d45bfc4594

tasks/spec.rake
@@ -17,7 +17,7 @@ begin
   namespace :spec do
     desc 'Run unit specs'
     RSpec::Core::RakeTask.new(:unit) do |task|
-      task.pattern = 'spec/unit/*/**/*_spec.rb'
+      task.pattern = 'spec/unit/**/*_spec.rb'
     end
 
     desc 'Run integration specs'