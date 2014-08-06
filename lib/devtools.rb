0903c59391eb6bc830ddac96a731500a74881741
Markus Schirp <mbj@schirp-dso.com>
2014-08-06 17:32:06 +0000

    Add Devtools.{ci,circle_ci,travis_ci}? predicates

--- lib/devtools.rb
+++ lib/devtools.rb
@@ -143,4 +143,34 @@ module Devtools
     self
   end
 
+  # Detect ci
+  #
+  # @return [Boolean]
+  #
+  # @api private
+  #
+  def self.ci?
+    ENV.key?('CI')
+  end
+
+  # Detect circle ci
+  #
+  # @return [Boolean]
+  #
+  # @api private
+  #
+  def self.circle_ci?
+    ENV.key?('CIRCLECI')
+  end
+
+  # Detect travis ci
+  #
+  # @return [Boolean]
+  #
+  # @api private
+  #
+  def self.travis?
+    ENV.key?('TRAVIS')
+  end
+
 end # module Devtools
