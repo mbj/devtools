8a67b03823794696d6682d78194962e3cdab70c1
John Backus <jbackus@stanford.edu>
2015-10-07 09:17:47 -0700

    Add documentation to `Rake::Flay`

--- lib/devtools/rake/flay.rb
+++ lib/devtools/rake/flay.rb
@@ -1,6 +1,6 @@
 module Devtools
   module Rake
-    # :reek:IrresponsibleModule
+    # Flay metric runner
     class Flay
       include Anima.new(:threshold, :total_score, :files), Procto.call(:verify), Adamantium
 
@@ -8,14 +8,32 @@ module Devtools
       TOTAL_MISMATCH  = 'Flay total is now %d, but expected %d'.freeze
       ABOVE_THRESHOLD = '%d chunks have a duplicate mass > %d'.freeze
 
+      # Initialize flay task settings
+      #
       # disable :reek:UtilityFunction
+      #
+      # @param options [Hash] flay options
+      # @option threshold [Integer] :threshold maximum flay threshold
+      # @option total_score [Integer] :total_score maximum allowed mass
+      # @option directories [Array<String>] :directories directories for flay
+      #
+      # @return [undefined]
+      #
+      # @api private
       def initialize(options)
         config = options.dup
         directories = config.delete(:directories)
         super(config.merge(files: ::Flay.expand_dirs_to_files(directories)))
       end
 
+      # Verify code specified by `files` does not violate flay expectations
+      #
+      # @raise [SystemExit] if a violation is found
+      # @return [undefined] otherwise
+      #
       # rubocop:disable MethodLength
+      #
+      # @api private
       def verify
         # Run flay first to ensure the max mass matches the threshold
         Devtools.notify_metric_violation(
@@ -37,35 +55,75 @@ module Devtools
 
     private
 
+      # Is there mass duplication which exceeds specified `threshold`
+      #
+      # @return [Boolean]
+      #
+      # @api private
       def above_threshold?
         restricted_mass_size.nonzero?
       end
 
+      # Is the specified `threshold` greater than the largest flay mass
+      #
+      # @return [Boolean]
+      #
+      # @api private
       def below_threshold?
         threshold > largest_mass
       end
 
+      # Is the expected mass total different from the actual mass total
+      #
+      # @return [Boolean]
+      #
+      # @api private
       def total_mismatch?
         !total_mass.equal?(total_score)
       end
 
+      # Size of mass measured by `Flay::Scale` and filtered by `threshold`
+      #
+      # @return [Integer]
+      #
+      # @api private
       def restricted_mass_size
         restricted_flay_scale.measure.size
       end
 
+      # Sum of all flay mass
+      #
+      # @return [Integer]
+      #
+      # @api private
       def total_mass
         flay_masses.reduce(:+).to_i
       end
 
+      # Largest flay mass found
+      #
+      # @return [Integer]
+      #
+      # @api private
       def largest_mass
         flay_masses.max.to_i
       end
 
+      # Flay scale which only measures mass above `threshold`
+      #
+      # @return [Flay::Scale]
+      #
+      # @api private
       def restricted_flay_scale
         Devtools::Flay::Scale.new(minimum_mass: threshold.succ, files: files)
       end
       memoize :restricted_flay_scale
 
+      # All flay masses found in `files`
+      #
+      # @return [Array<Rational>]
+      #
+      # @api private
       def flay_masses
         Devtools::Flay::Scale.call(minimum_mass: 0, files: files)
       end
