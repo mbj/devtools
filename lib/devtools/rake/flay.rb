345dffd22d358205dd09291d3f5cfa0dfc3a79b6
John Backus <jbackus@stanford.edu>
2015-10-06 22:50:46 -0700

    Refactor `Rake::Flay` to extract methods

--- lib/devtools/rake/flay.rb
+++ lib/devtools/rake/flay.rb
@@ -2,7 +2,7 @@ module Devtools
   module Rake
     # :reek:IrresponsibleModule
     class Flay
-      include Anima.new(:threshold, :total_score, :files), Procto.call(:verify)
+      include Anima.new(:threshold, :total_score, :files), Procto.call(:verify), Adamantium
 
       # disable :reek:UtilityFunction
       def initialize(options)
@@ -11,31 +11,46 @@ module Devtools
         super(config.merge(files: ::Flay.expand_dirs_to_files(directories)))
       end
 
-      # rubocop:disable AbcSize, MethodLength, GuardClause
+      # rubocop:disable MethodLength, GuardClause
       # disable :reek:DuplicateMethodCall :reek:TooManyStatements
       def verify
         # Run flay first to ensure the max mass matches the threshold
-        masses = Devtools::Flay::Scale.call(minimum_mass: 0, files: files)
-
-        max = masses.max.to_i
-        unless max >= threshold
-          Devtools.notify_metric_violation "Adjust flay threshold down to #{max}"
+        unless largest_mass >= threshold
+          Devtools.notify_metric_violation "Adjust flay threshold down to #{largest_mass}"
         end
 
-        total = masses.inject(:+).to_i
-        unless total.equal?(total_score)
-          Devtools.notify_metric_violation "Flay total is now #{total}, but expected #{total_score}"
+        unless total_mass.equal?(total_score)
+          Devtools.notify_metric_violation "Flay total is now #{total_mass}, but expected #{total_score}"
         end
 
         # Run flay a second time with the threshold set
-        scale = Devtools::Flay::Scale.new(minimum_mass: threshold.succ, files: files)
-        mass_size = scale.measure.size
+        mass_size = restricted_flay_scale.measure.size
 
         if mass_size.nonzero?
-          scale.flay_report
+          restricted_flay_scale.flay_report
           Devtools.notify_metric_violation "#{mass_size} chunks have a duplicate mass > #{threshold}"
         end
       end
+
+    private
+
+      def total_mass
+        flay_masses.reduce(:+).to_i
+      end
+
+      def largest_mass
+        flay_masses.max.to_i
+      end
+
+      def restricted_flay_scale
+        Devtools::Flay::Scale.new(minimum_mass: threshold.succ, files: files)
+      end
+      memoize :restricted_flay_scale
+
+      def flay_masses
+        Devtools::Flay::Scale.call(minimum_mass: 0, files: files)
+      end
+      memoize :flay_masses
     end
   end
 end
