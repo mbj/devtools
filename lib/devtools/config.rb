ac80555b868182b404993aeff4893b57785f5856
Markus Schirp <mbj@schirp-dso.com>
2015-10-26 22:29:45 +0000

    Refactor config to allow value checks
    * Backfill value checks for all configs

--- lib/devtools/config.rb
+++ lib/devtools/config.rb
@@ -2,25 +2,60 @@ module Devtools
 
   # Abstract base class of tool configuration
   class Config
-    include AbstractType, Concord.new(:project)
+    include Adamantium::Flat, AbstractType, Concord.new(:project)
 
     # Represent no configuration
     DEFAULT_CONFIG = {}.freeze
 
+    # Simple named type check representation
+    class TypeCheck
+      # Type check against expected class
+      include Concord.new(:name, :allowed_classes)
+
+      ERROR_FORMAT = '%<name>s: Got instance of %<got>s expected %<allowed>s'.freeze
+      CLASS_DELIM  = ','.freeze
+
+      # Check value for instance of expected class
+      #
+      # @param [Object] value
+      #
+      # @return [Object]
+      def call(value)
+        klass = value.class
+
+        unless allowed_classes.any?(&klass.method(:equal?))
+          fail TypeError, ERROR_FORMAT % {
+            name:    name,
+            got:     klass,
+            allowed: allowed_classes.join(CLASS_DELIM)
+          }
+        end
+
+        value
+      end
+    end # TypeCheck
+
+    private_constant(*constants(false))
+
+    # Error raised on type errors
+    TypeError = Class.new(RuntimeError)
+
     # Declare an attribute
     #
     # @param [Symbol] name
-    #
-    # @yieldreturn [Object]
-    #   the default value to use
+    # @param [Array<Class>] classes
     #
     # @api private
     #
     # @return [self]
     #
-    def self.attribute(name, *default)
+    def self.attribute(name, classes, **options)
+      default    = [options.fetch(:default)] if options.key?(:default)
+      type_check = TypeCheck.new(name, classes)
+      key        = name.to_s
+
       define_method(name) do
-        raw.fetch(name.to_s, *default)
+        type_check.call(raw.fetch(key, *default))
       end
     end
     private_class_method :attribute
@@ -32,8 +67,9 @@ module Devtools
     # @api private
     #
     def config_file
-      @config_file ||= project.config_dir.join(self.class::FILE).freeze
+      project.config_dir.join(self.class::FILE)
     end
+    memoize :config_file
 
   private
 
@@ -44,8 +80,9 @@ module Devtools
     # @api private
     #
     def raw
-      @raw ||= yaml_config || DEFAULT_CONFIG
+      yaml_config || DEFAULT_CONFIG
     end
+    memoize :raw
 
     # Return the raw config data from a yaml file
     #
@@ -57,8 +94,7 @@ module Devtools
     # @api private
     #
     def yaml_config
-      config_file = self.config_file
-      YAML.load_file(config_file).freeze if config_file.file?
+      IceNine.deep_freeze(YAML.load_file(config_file)) if config_file.file?
     end
 
     # Rubocop configuration
@@ -77,10 +113,10 @@ module Devtools
       DEFAULT_LIB_DIRS = %w[lib].freeze
       DEFAULT_EXCLUDES = %w[].freeze
 
-      attribute :total_score
-      attribute :threshold
-      attribute :lib_dirs, DEFAULT_LIB_DIRS
-      attribute :excludes, DEFAULT_EXCLUDES
+      attribute :total_score, [Fixnum]
+      attribute :threshold,   [Fixnum]
+      attribute :lib_dirs,    [Array], default: DEFAULT_LIB_DIRS
+      attribute :excludes,    [Array], default: DEFAULT_EXCLUDES
     end # Flay
 
     # Yardstick configuration
@@ -111,9 +147,9 @@ module Devtools
       FILE             = 'flog.yml'.freeze
       DEFAULT_LIB_DIRS = %w[lib].freeze
 
-      attribute :total_score
-      attribute :threshold
-      attribute :lib_dirs, DEFAULT_LIB_DIRS
+      attribute :total_score, [Float]
+      attribute :threshold,   [Float]
+      attribute :lib_dirs,    [Array], default: DEFAULT_LIB_DIRS
     end # Flog
 
     # Mutant configuration
@@ -122,13 +158,13 @@ module Devtools
       DEFAULT_NAME     = ''.freeze
       DEFAULT_STRATEGY = 'rspec'.freeze
 
-      attribute :name,            DEFAULT_NAME
-      attribute :strategy,        DEFAULT_STRATEGY
-      attribute :zombify,         false
-      attribute :since,           nil
-      attribute :ignore_subjects, []
-      attribute :expect_coverage, 1
-      attribute :namespace
+      attribute :name,            [String],                default: DEFAULT_NAME
+      attribute :strategy,        [String],                default: DEFAULT_STRATEGY
+      attribute :zombify,         [TrueClass, FalseClass], default: false
+      attribute :since,           [String],                default: nil
+      attribute :ignore_subjects, [Array],                 default: []
+      attribute :expect_coverage, [Float],                 default: 1.0
+      attribute :namespace,       [String]
     end # Mutant
 
     # Devtools configuration
@@ -136,7 +172,7 @@ module Devtools
       FILE                      = 'devtools.yml'.freeze
       DEFAULT_UNIT_TEST_TIMEOUT = 0.1  # 100ms
 
-      attribute :unit_test_timeout, DEFAULT_UNIT_TEST_TIMEOUT
+      attribute :unit_test_timeout, [Float], default: DEFAULT_UNIT_TEST_TIMEOUT
     end # Devtools
   end # Config
 end # Devtools
---
+++ spec/unit/devtools/config_spec.rb
@@ -0,0 +1,85 @@
+RSpec.describe Devtools::Config do
+
+  describe '.attribute' do
+    let(:raw) do
+      {
+        'a' => 'bar',
+        'c' => []
+      }
+
+    end
+
+    let(:config_path) { instance_double(Pathname) }
+
+    let(:class_under_test) do
+      expect(config_path).to receive(:file?).and_return(file?)
+
+      expect(project.config_dir).to receive(:join)
+        .with('bar.yml')
+        .and_return(config_path)
+
+      expect(config_path).to receive(:frozen?).and_return(true)
+
+      Class.new(described_class) do
+        attribute :a, [String]
+        attribute :b, [Array], default: []
+        attribute :c, [TrueClass, FalseClass]
+
+        const_set(:FILE, 'bar.yml')
+      end
+    end
+
+    let(:project) do
+      instance_double(
+        Devtools::Project,
+        config_dir: Pathname.new('project-config')
+      )
+    end
+
+    subject do
+      class_under_test.new(project)
+    end
+
+    context 'on present config' do
+      let(:class_under_test) do
+        # Setup message expectation in a lasy way, not in a before
+        # block to make sure the around hook setting timeouts from the
+        # code under test is not affected.
+        expect(YAML).to receive(:load_file)
+          .with(config_path)
+          .and_return(raw)
+
+        expect(IceNine).to receive(:deep_freeze)
+          .with(raw)
+          .and_return(raw)
+
+        super()
+      end
+
+      let(:file?) { true }
+
+      it 'allows to receive existing keys' do
+        expect(subject.a).to eql('bar')
+      end
+
+      it 'allows to receive absent keys with defaults' do
+        expect(subject.b).to eql([])
+      end
+
+      it 'executes checks when configured' do
+        expect { subject.c }.to raise_error(
+          Devtools::Config::TypeError,
+          'c: Got instance of Array expected TrueClass,FalseClass'
+        )
+      end
+    end
+
+    context 'on absent config' do
+      let(:file?) { false }
+
+      it 'defaults to absent keys' do
+        expect(subject.b).to eql([])
+      end
+    end
+  end
+end
