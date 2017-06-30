c6732d0936578226ea6b8149e50c6bfba23de923
Bartek Bułat <bartek.bulat@gmail.com>
2017-06-30 11:06:19 +0200

    Suppress Ruby 2.4 deprecation warnings re Fixnum
    - Update SimpleCov to v0.14.1, which fixes warnings coming from that gem
    - Use 0.class as integer class indicator, it will work on both, 2.3 and
      2.4.
    - Remove unused shared/spec/shared/hash_method_behavior.rb file.
    - Update Ruby version on CircleCI to 2.4.1
    - Update reek gem dependency to ~> 4.6.0, to relax implicit constraint
      on parser version.
    - Disable RuboCop FrozenStringLiteralComment cop
    - Increase Flay total_score to 115

--- shared/spec/shared/hash_method_behavior.rb
+++
@@ -1,7 +0,0 @@
-shared_examples_for 'a hash method' do
-  it_should_behave_like 'an idempotent method'
-
-  it 'is a Fixnum' do
-    should be_instance_of(Fixnum) # rubocop:disable Lint/UnifiedInteger
-  end
-end
